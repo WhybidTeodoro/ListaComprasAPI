86dbc524c2864ede5a9f30a6eac23a6291a43d24
WhybidTeodoro <twhybid@gmail.com>
2025-12-19 20:57:26 -0300

    Adicionado User Entity

--- ListaComprasAPI/Data/AppDbContext.cs
+++ ListaComprasAPI/Data/AppDbContext.cs
@@ -1,12 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ListaComprasAPI.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace ListaComprasAPI.Data;
 
 //Dbcontext principal responsavel por gerenciar a conexão com o banco e mapear as entidades
 public class AppDbContext : DbContext
 {
-	public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-	{
+	public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-	}
+	//Tabela de usuarios do sistema.
+	DbSet<User> Users { get; set; }
 }
---
+++ ListaComprasAPI/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿namespace ListaComprasAPI.Entities;
+
+//Representa o Usuario do sistema. Cada usuario possui sua lista de compras
+public class User
+{
+    //Identificador unico do usuario no banco de dados
+    public int Id { get; set; }
+
+    //Nome do Usuario
+    public string Name { get; set; } = string.Empty;
+
+    //Email do usuario. Será usado para o login.
+    public string Email { get; set; } = string.Empty;
+
+    //Hash da senha do usuario
+    public string PasswordHash { get; set; } = string.Empty;
+
+    //Data da criação do usuario
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+}

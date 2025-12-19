d0d2dca5413660e557b9a626eba7ecbc20f9a015
WhybidTeodoro <twhybid@gmail.com>
2025-12-19 20:06:27 -0300

    Configuração EF Core DbContext com MySql

---
+++ ListaComprasAPI/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ListaComprasAPI.Data;
+
+//Dbcontext principal responsavel por gerenciar a conexão com o banco e mapear as entidades
+public class AppDbContext : DbContext
+{
+	public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+	{
+
+	}
+}
--- ListaComprasAPI/Program.cs
+++ ListaComprasAPI/Program.cs
@@ -1,9 +1,21 @@
+using ListaComprasAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+    options.UseMySql(
+        connectionString,
+        ServerVersion.AutoDetect(connectionString)
+    );
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 

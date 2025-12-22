084d7393151da19530baf38362cccb4fe9e4f44a
WhybidTeodoro <twhybid@gmail.com>
2025-12-22 08:59:42 -0300

    Adicionando Dtos para ShoppingList

---
+++ ListaComprasAPI/DTOs/ShoppingList/CreateShoppingListDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ListaComprasAPI.DTOs.ShoppingList;
+
+/// <summary>
+/// Dto responsavel por criar uma lista de compras
+/// </summary>
+public class CreateShoppingListDto
+{
+    //Nome da lista
+    [Required]
+    [MaxLength(100)]
+    public string Name { get; set; } = string.Empty;
+}
---
+++ ListaComprasAPI/DTOs/ShoppingList/ShoppingListResponseDto.cs
@@ -0,0 +1,13 @@
+﻿namespace ListaComprasAPI.DTOs.ShoppingList;
+
+/// <summary>
+/// Dto usado para retorno da lista de compras
+/// </summary>
+public class ShoppingListResponseDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    //Valor total calculado dinamicamente
+    public decimal TotalValue { get; set; }
+}
